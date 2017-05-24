2dc2ede18210b56202f70377890bf0d750a92315
Gustavo Santos <gusanthiagodv@gmail.com>
2017-05-24 01:05:05 -0300

    Parseando imagem para exibir

--- index.rb
+++ index.rb
@@ -1,11 +1,19 @@
 require 'telegram/bot'
+require 'net/http'
 
-token = ''
+token = '374527114:AAH2irPyZF-FnfOWGorCoodW4kVJNFEFolY'
+# urlRaspberry = 'http://192.168.43.138:8080/RPi_Cam/cam.jpg'
+urlTest = 'https://lh6.googleusercontent.com/-ztabNasVSSU/TWl43mOmsPI/AAAAAAAAAIA/_fKSb-CC9Ss/s400/030120091293.jpg' # em teste
+uriApi = URI('http://localhost:3000/v1/api/image/create/')
+
+# TODO fazer um autoload :|
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+  	# TODO Melhorar interface das mensagens
     case message.text
-    when '/tembreja'
-      bot.api.send_message(chat_id: message.chat.id, text:"http://192.168.43.138:8080/RPi_Cam/cam.jpg")
+    when '/disponivel'
+        res = JSON.parse(Net::HTTP.post_form(uriApi, 'urlImage' => urlTest).body)
+      	bot.api.send_message(chat_id: message.chat.id, text: res['urlOrigin'])
     end
   end
 end
