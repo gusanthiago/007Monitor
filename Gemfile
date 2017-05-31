a34da21884d1e716aa9311266f12375c6df1f12d
Gustavo Santos <gusanthiagodv@gmail.com>
2017-05-30 21:25:57 -0300

    Adicionado Gemfile

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org' do 
+  ruby '2.4.0' 
+  gem 'telegram-bot-ruby'
+end
--- index.rb
+++ index.rb
@@ -4,16 +4,17 @@ require 'net/http'
 token = '374527114:AAH2irPyZF-FnfOWGorCoodW4kVJNFEFolY'
 # urlRaspberry = 'http://192.168.43.138:8080/RPi_Cam/cam.jpg'
 urlTest = 'https://lh6.googleusercontent.com/-ztabNasVSSU/TWl43mOmsPI/AAAAAAAAAIA/_fKSb-CC9Ss/s400/030120091293.jpg' # em teste
-uriApi = URI('http://localhost:3000/v1/api/image/create/')
-
+urlServer = 'http://localhost:3000'
+urlApi = '/v1/api/'
+urlCreate = URI(urlServer + urlApi + 'image/create/')
 # TODO fazer um autoload :|
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
   	# TODO Melhorar interface das mensagens
     case message.text
     when '/disponivel'
-        res = JSON.parse(Net::HTTP.post_form(uriApi, 'urlImage' => urlTest).body)
-      	bot.api.send_message(chat_id: message.chat.id, text: res['urlOrigin'])
+        res = JSON.parse(Net::HTTP.post_form(urlCreate, 'urlImage' => urlTest).body)
+      	bot.api.sendPhoto(chat_id: message.chat.id, photo: 'http://localhost:3000/images_request/240343359710988295606533978191045067684JPEDUQGB.jpg' )
     end
   end
 end
